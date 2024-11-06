644d244569d052aa60d2a589829072c99e2d73f9
Arufa Khanom <arufamanar1@gmail.com>
2024-11-05 20:33:09 -0500

    adding files for checklist app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import CreateChecklist from './components/CreateChecklist';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create-checklist" element={<CreateChecklist />} />
+      </Routes>
+    </Router>
+  );
+}
